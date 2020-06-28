8ce99741476f4bc1d01582251b55dd499c4de7f5
Jop Middelkamp <middelkamp@live.nl>
2020-06-28 02:06:24 +0200

    Added MainPage.xaml to the MvvmCross sample project.

---
+++ Samples/XamarinFormsMvvmCrossSample/XamarinFormsMvvmCrossSample/Pages/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using MvvmCross.Forms.Views;
+using XamarinFormsMvvmCrossSample.Core.ViewModels;
+
+namespace XamarinFormsMvvmCrossSample.Pages
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MvxContentPage<MainViewModel>
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
